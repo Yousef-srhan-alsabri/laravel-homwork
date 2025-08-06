9ec77e2e7040815ea171483c0addb5c6c99a1cb0
Yousef-srhan-alsabri <Yousefsrhan777@gmail.com>
2025-08-07 02:55:47 +0300

    Initial commit

---
+++ h1/blog/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+use App\Http\Controllers\PageController;
+
+
+
+Route::get('/welcome', [PageController::class, 'welcome']);
